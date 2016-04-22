e05aa3101129b49a846efed1bac15707e57362d7
lekkerbit <lekkerbit@gmail.com>
2016-04-22 18:31:40 +1000

    Create: Agent.findBorderTiles() Agent.findConsecutivePathes()

--- src/Agent.py
+++ src/Agent.py
@@ -2,40 +2,84 @@ import socket, threading
 
 
 
+class Tile:
+    def __init__(self, x, y, type = 'N'):
+        self.__x = x
+        self.__y = y
+        self.__north = None
+        self.__east  = None
+        self.__west  = None
+        self.__south = None
+        self.__type  = type
+        self.__walked = False
 
+    def pos(self):
+        return (self.__x, self.__y)
+
+    def getNorth(self):
+        return self.__north
+
+    def getEast(self):
+        return self.__east
+
+    def getWest(self):
+        return self.__west
+
+    def getSouth(self):
+        return self.__south
+
+    def getType(self):
+        return self.__type
+
+    def walked(self):
+        self.walked = True
+
+    def setType(self, type):
+        self.__type = type
+
+    def setNorth(self, tile):
+        self.__north = tile
+
+    def setWest(self, tile):
+        self.__west = tile
+
+    def setEast(self, tile):
+        self.__east = tile
+
+    def setSouth(self, tile):
+        self.__south = tile
 
 class Map:
-    class tile:
-        def __init__(self, x, y, type = None):
-            self.north = None
-            self.east  = None
-            self.west  = None
-            self.south = None
-            self.type  = type
-
-        def pos(self):
-            return (self.x, self.y)
-
     def __init__(self, dimension = 80):
         self.ground = dict()
         for i in range(-dimension, dimension + 1):
             for j in range(-dimension, dimension + 1):
-                self.ground[(i,j)] = self.tile(i, j)
+                self.ground[(i,j)] = Tile(i, j)
 
         for i in range(-dimension, dimension + 1):
             for j in range(-dimension, dimension + 1):
                 if (i,j-1) in self.ground:
-                    self.ground[(i,j)].north = self.ground[(i,j-1)]
+                    self.ground[(i,j)].setNorth(self.ground[(i,j-1)])
                 if (i+1,j) in self.ground:
-                    self.ground[(i,j)].east  = self.ground[(i+1,j)]
+                    self.ground[(i,j)].setEast(self.ground[(i+1,j)])
                 if (i-1,j) in self.ground:
-                    self.ground[(i,j)].west  = self.ground[(i-1,j)]
+                    self.ground[(i,j)].setWest(self.ground[(i-1,j)])
                 if (i,j+1) in self.ground:
-                    self.ground[(i,j)].south = self.ground[(i,j+1)]
+                    self.ground[(i,j)].setSouth(self.ground[(i,j+1)])
 
     def addTile(self, x, y, type):
         if type != '^':
-            self.ground[(x, y)] = type
+            self.ground[(x, y)].setType(type)
+        else:
+            self.ground[(x, y)].walked()
+            if self.ground[(x, y)].getType() == 'N':
+                self.ground[(x, y)].setType(' ')
+
+    def getTile(self, x, y):
+        if (x, y) in self.ground:
+            return self.ground[(x, y)]
+        else:
+            return None
 
 
 
@@ -46,15 +90,10 @@ class Agent:
         self.y = 0
         self.facing = 'S'
         self.map = Map()
-        self.pipe = Pipe(port)
+        # self.pipe = Pipe(port)
         self.hasStone = 0
         self.hasAxe = 0
         self.hasKey = 0
-        initMap = self.pipe.receive()
-        turnedMap = self.turnMapToNorth(initMap)
-        for i in range(-self.SIGHT//2+1, self.SIGHT//2+1):
-            for j in range(-self.SIGHT//2+1, self.SIGHT//2+1):
-                self.map.addTile(i, j, turnedMap[i+self.SIGHT//2][j+self.SIGHT//2])
 
     def turnMapToNorth(self, map):
         tmpmap = map
@@ -75,28 +114,116 @@ class Agent:
         return tuple(zip(*map))
 
     def start(self):
-        while(True):
-            goalPath = self.findGoldPath()
-            nearestUnknow = self.findNearestUnknown()
-            if goalPath:
-                self.goTo(goalPath)
-                print('Victory')
-                break
-            elif nearestUnknow:
-                self.goTo(nearestUnknow)
-            else:
-                print('No Path Found')
-                break
+        # initMap = self.pipe.receive()
+        # print(initMap)
+        initMap = [[' ', ' ', 'o', ' ', ' '],
+                   [' ', ' ', ' ', ' ', ' '],
+                   [' ', ' ', '^', ' ', ' '],
+                   [' ', ' ', ' ', ' ', ' '],
+                   [' ', ' ', ' ', ' ', ' ']]
+        turnedMap = self.turnMapToNorth(initMap)
+        for i in range(self.SIGHT):
+            for j in range(self.SIGHT):
+                # print(i - self.SIGHT//2, j - self.SIGHT//2, turnedMap[i][j])
+                self.map.addTile(i - self.SIGHT//2, j - self.SIGHT//2, turnedMap[i][j])
+        # self.traversal()
+        # while(True):
+        #     goalPath = self.findGoldPath()
+        #     nearestUnknow = self.findAnUnknown()
+        #     if goalPath:
+        #         self.goTo(goalPath)
+        #         print('Victory')
+        #         break
+        #     elif nearestUnknow:
+        #         self.goTo(nearestUnknow)
+        #     else:
+        #         print('No Path Found')
+        #         break
+        # print(b)
 
-    def findNearestUnknown(self):
-        
+        borderTiles = self.findBorderTiles()
+        consecutivePathes = self.findConsecutivePathes(borderTiles)
+        # print(consecutivePathes)
+        # print(b)
+        print([(x.pos(), x.getType()) for y in consecutivePathes for x in y])
+
+        # for t in borderTiles:
+        #     self.gotoWithCare(t)
+
+    # def traversal(self):
+    #     borderMap = self.getBorderMap(self)
+    #     originTile = self.map.getTile(self.x, self.y)
+
+
+    # def findAnUnknown(self):
+    #     originTile = self.map.getTile(self.x, self.y)
+    #     stack = [originTile]
+    #     wentTile = [originTile]
+    #     while(stack):
+    #         if originTile.getNorth() not in wentTile:
 
 
 
+    def DFS(self, x, y, stack):
+        pass
 
 
+    def findBorderTiles(self):
+        x, y = self.x, self.y
+        walkable = [' ', 'O', 'k', 'a', '^']
+        walked = [self.map.getTile(x, y)]
+        stack = [self.map.getTile(x, y)]
+        borderTile = []
+        while(stack):
+            tile = stack.pop()
+            walked.append(tile)
+            if tile.getType() not in walkable:
+                continue
+            north = tile.getNorth()
+            west = tile.getWest()
+            south = tile.getSouth()
+            east = tile.getEast()
+            if any([x.getType() not in walkable for x in [north, west, south, east]]):
+                borderTile.append(tile)
 
+            if north not in walked:
+                stack.append(north)
+            if west not in walked:
+                stack.append(west)
+            if south not in walked:
+                stack.append(south)
+            if east not in walked:
+                stack.append(east)
+        return borderTile
 
+    def findConsecutivePathes(self, tiles):
+        pathes = [[tiles[0]]]
+        for i in range(1,len(tiles)):
+            pos1 = tiles[i].pos()
+            pos2 = tiles[i-1].pos()
+            distance = abs(pos1[0]-pos2[0]) + abs(pos1[1]-pos2[1])
+            if distance != 1:
+                pathes.append([])
+            pathes[-1].append(tiles[i])
+        return pathes
+
+    # def gotoWithCare(self, tile):
+    #     walkable = [' ', 'O', 'k', 'a', '^']
+    #     pathes = [[tile]]
+    #     walked = [tile]
+    #     while(True):
+    #         newPaht = []
+    #         for path in pathes:
+    #             tail = path[-1]
+    #             if tail in walked:
+    #
+    #             north = tail.getNorth()
+    #             east = tail.getEast()
+    #             south = tail.getSouth()
+    #             west = tail.getWest()
+
+    # def goAlong(self, path):
+    #     self.goWithCare(path[0])
 
 
 class Pipe:
@@ -155,6 +282,7 @@ if __name__ == '__main__':
 
     # p = Pipe(31415)
     a = Agent(31415)
+    a.start()
     # print(p.receive())
     # order = ['f', 'f', 'r', 'r', 'f', 'f', 'r', 'f', 'f', 'f', 'f', 'l', 'f', 'f','r', 'f', 'f']
     # for o in order:
